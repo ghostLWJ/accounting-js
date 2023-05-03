6953bc25ec5ec60511bd0cbe24d5ef490b376940
ghostLWJ <ghostlwj@gmail.com>
2023-05-04 00:56:31 +0800

    feat: using BigNumber.js package for high-precision arithmetic

--- tests/toFixed.js
+++ tests/toFixed.js
@@ -12,3 +12,42 @@ test('should round', t => {
   t.is(toFixed(3.1415, 2, 1), '3.15');
   t.is(toFixed(12.56, 1, -1), '12.5');
 });
+
+test('should correctly round values with high precision', t => {
+  t.is(toFixed(0.6149, 2), '0.61');
+  t.is(toFixed(1234567.89123, 5), '1234567.89123');
+  t.is(toFixed(0.1 + 1e-10, 8), '0.10000000');
+});
+
+test('should handle very small numbers', t => {
+  t.is(toFixed(1e-10, 10), '0.0000000001');
+  t.is(toFixed(1e-10, 12), '0.000000000100');
+  t.is(toFixed(1e-10 + 2e-10, 10), '0.0000000003');
+});
+
+test('should correctly apply custom rounding', t => {
+  t.is(toFixed(0.615, 2, 0), '0.62');
+  t.is(toFixed(0.615, 2, 1), '0.62');
+  t.is(toFixed(0.615, 2, -1), '0.61');
+  t.is(toFixed(-0.615, 2, 0), '-0.62');
+  t.is(toFixed(-0.615, 2, 1), '-0.61');
+  t.is(toFixed(-0.615, 2, -1), '-0.62');
+});
+
+test('should handle very small positive numbers', t => {
+  t.is(toFixed(1e-12, 12), '0.000000000001');
+  t.is(toFixed(1e-12, 14), '0.00000000000100');
+  t.is(toFixed(1e-12 + 2e-12, 12), '0.000000000003');
+  t.is(toFixed(9e-13, 12), '0.000000000001');
+  t.is(toFixed(0.00000012345, 9), '0.000000123');
+  t.is(toFixed(0.00000012345, 11), '0.00000012345');
+});
+
+test('should handle very small negative numbers', t => {
+  t.is(toFixed(-1e-12, 12), '-0.000000000001');
+  t.is(toFixed(-1e-12, 14), '-0.00000000000100');
+  t.is(toFixed(-1e-12 - 2e-12, 12), '-0.000000000003');
+  t.is(toFixed(-9e-13, 12), '-0.000000000001');
+  t.is(toFixed(-0.00000012345, 9), '-0.000000123');
+  t.is(toFixed(-0.00000012345, 11), '-0.00000012345');
+});
